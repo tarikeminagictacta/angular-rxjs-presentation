9e161a9a303964fcaad2b470f14aebcbdd1fdba7
Tarik Eminagić <eminagic.tarik@gmail.com>
2020-06-19 00:33:25 +0200

    removed playground

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
-  { path: 'playground', loadChildren: () => import('./playground/playground.module').then((m) => m.PlaygroundModule) },
   { path: 'agenda', loadChildren: () => import('./agenda/agenda.module').then((m) => m.AgendaModule) },
   { path: 'topics', loadChildren: () => import('./topic/topic.module').then((m) => m.TopicModule) },
 ];
